5ce25ef311bc91b6f8aae3632c5a7f4a74805dfd
dani1150 <daniellerbrown07@gmail.com>
2025-01-12 17:11:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const overlay = document.querySelector('.overlay');
+
+document.addEventListener('mousemove', (e) => {
+  overlay.style.maskImage = `radial-gradient(circle at ${e.clientX}px ${e.clientY}px, rgba(0, 0, 0, 0) 100px, rgba(0, 0, 0, 1) 250px)`;
+  overlay.style.webkitMaskImage = `radial-gradient(circle at ${e.clientX}px ${e.clientY}px, rgba(0, 0, 0, 0) 100px, rgba(0, 0, 0, 1) 250px)`;
+});
